238aa982f1653b67b8e0721863256828fdcb99f2
KantLee <78336221+cnkant@users.noreply.github.com>
2024-03-07 21:23:32 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=15567869&pid=278b5e384c1ac290a5b3fe558e21ddfc&tid=b5fbc020bbb3b7a7c85cf06b73a373ae",
+    "https://www.maimemo.com/share/page?uid=15567869&pid=71a93b75a4a016ad4ef6b8243b89d7a1&tid=4f8038c73181ee788efe326d78ecc9f8",
 
 
   ]
