b448336cec53bd88f2a17e9345d101f9437f41c8
KantLee <78336221+cnkant@users.noreply.github.com>
2024-03-31 08:20:00 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -5,10 +5,8 @@
 
 
 const MMSLS = [
-    
-    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
-    "https://www.maimemo.com/share/page?uid=32454734&pid=51406dd9e063b581acceb3363d60e3c3&tid=e22a83446318a25969f7f24f913fa60c",
-    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
+ "https://www.maimemo.com/share/page?uid=15567869&pid=01c723220cf8d9e859f6239a2c9c94f3&tid=c6b9d87b1b6bcb1d3f8f597f2bae83ff"
+
   ]
 =====================以上为范例！=====================
 
